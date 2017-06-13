bcd7778023a993909d089429212b68cd53550b51
Udem <udem@fedora.mshome.net>
2017-06-13 14:17:33 -0600

    tarea

---
+++ tarea.py
@@ -0,0 +1,6 @@
+nombre = input("Digita tu nombre: ")
+apellido = input("Digita tu apellido: ")
+edad = int(input("Digita tu edad: "))
+cumple = 100 - edad 
+cumplira = 2017 + cumple
+print ("<<", nombre,">> , cumpliras 100 años en el:",str(cumplira))

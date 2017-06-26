9cd24bb2e0f5c87258c3297daae772899542b5dd
Udem <udem@fedora.mshome.net>
2017-06-26 16:35:42 -0600

    prueba

--- prueba.py
+++ prueba.py
@@ -8,7 +8,7 @@
 #mprimir longitud de cadena
 #Reemplazar en la cadena la letra o por 
 
-dato = (input("Ingrese un texto: "))
+dato = raw_input("Ingrese un texto: ")
 texto = dato
 cuenta = 0
 buscar = "o"
@@ -16,8 +16,8 @@ reemplazar_por= 0
 for carac in texto:
     if carac == 'e':
         cuenta += 1
-print ("El texto tiene ",cuenta ," Caracteres de e")
-print("capitalizacion: ", texto.upper())
-print("La longitud de la cadena es de: ", len(dato))
-print("Reemplazando o por 0 : ", dato.replace(buscar,str(reemplazar_por)))
+print "El texto tiene ",cuenta ," Caracteres de e"
+print "capitalizacion: ", texto.upper()
+print "La longitud de la cadena es de: ", len(dato)
+print "Reemplazando o por 0 : ", dato.replace(buscar,str(reemplazar_por))
 

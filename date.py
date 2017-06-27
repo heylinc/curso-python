f8e7c58d9b69693df625d56575dfc66874ebe99a
Udem <udem@fedora.mshome.net>
2017-06-27 16:59:22 -0600

    tarea final numero 3

--- date.py
+++ date.py
@@ -1,5 +1,5 @@
 import random
-
+mensaje= "{0} de {1} de edad, es de la ciudad de {2} y cursa {3} de universidad"
 NOMBRES = [
     'Ana',
     'Pedro',
@@ -22,18 +22,41 @@ CIUDADES = [
     'Somoto',
     'Rivas'
 ]
+import random
 
 def generar_diccionario_estudiantes():
     estudiantes = {}
 
     for nombre in NOMBRES:
-        estudiantes= {
-            
-            'C De edad': random.randrange(16, 30),
-            'A El nombre del estudiante es': random.choice(NOMBRES),
-            'D y cursa el anio': random.randrange(1, 5),
-            'B es de la cuidad': random.choice(CIUDADES)
+        estudiantes[nombre] = {
+            'edad': random.randrange(16, 30),
+            'anio': random.randrange(1, 5),
+            'ciudad': random.choice(CIUDADES)
         }
-        return estudiantes
+    return estudiantes
 
-print generar_diccionario_estudiantes()
+
+if __name__ == '__main__':
+    diccio = generar_diccionario_estudiantes()
+ 
+    for llave,valor in diccio.iteritems() :
+        print llave,valor
+
+    for llave,valor in diccio.iteritems():
+        print mensaje.format(llave,valor['edad'],valor['ciudad'],valor['anio'])
+    print '\nEstudiantes de managua'
+    for llave,valor in diccio.iteritems():
+        if valor['ciudad'] == 'Managua':
+            print mensaje.format(llave,valor['edad'],valor['ciudad'],valor['anio'])
+        
+    print '\nEstudiantes de masaya que cursan 1er anio'
+    for llave,valor in diccio.iteritems():
+        if valor['ciudad'] == 'Masaya' and valor['anio'] == 1:
+            print mensaje.format(llave, valor['edad'], valor['ciudad'], valor['anio'])
+        
+    print '\nEstudiantes menores de 21'
+    for llave,valor in diccio.iteritems():
+        if valor['edad'] < 21:
+            msj = '{0} de {1} anios, es de la ciudad de {2} y cursa {3} de la universidad'
+            print msj.format(llave,valor['edad'],valor['ciudad'],valor['anio'])
+        

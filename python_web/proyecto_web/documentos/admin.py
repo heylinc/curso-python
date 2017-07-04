f3d24cc9b82ce7990a425b8f55ceda4ff051c530
Udem <udem@fedora.mshome.net>
2017-07-04 17:56:01 -0600

    proyecto web actual

--- python_web/proyecto_web/documentos/admin.py
+++ python_web/proyecto_web/documentos/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
-# Register your models here.
+#Importar modelo
+from documentos.models import Documento
+#crear subclase de admin.ModelAdmin
+class DocumentoAdmin(admin.ModelAdmin):
+	model = Documento
+#registrar modelAdmin para cada modelo
+admin.site.register(Documento,DocumentoAdmin)
--- python_web/proyecto_web/documentos/models.py
+++ python_web/proyecto_web/documentos/models.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Documento(models.Model):
-	edad = models.IntegerField(null=True, blank=True)
-	fecha = models.DateTimeField(null= True, blank= True)
+	#edad = models.IntegerField(null=True, blank=True)
+	#fecha = models.DateTimeField(null= True, blank= True)
+	nombre = models.CharField(max_length = 100, null = True, blank = True)
+	fecha = models.DateTimeField(null = True, blank = True)
+
 
 	def __unicode__(self):
 		return 'Documento - {0}'.format(self.id)
